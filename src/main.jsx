eb4958b37d270901b1da4d8a851467a014e40c98
vatiza <rakibur.mac@gmail.com>
2024-05-07 03:41:22 +0600

    assets add

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div className="">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  </React.StrictMode>
+);
