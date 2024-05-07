0bd454722558671c9e581602b95906a6e84e1c62
vatiza <rakibur.mac@gmail.com>
2024-05-07 17:13:53 +0600

    landing page design done

--- src/Components/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Components/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,13 +1,52 @@
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "@smastrom/react-rating/style.css";
+import { Pagination, Navigation } from "swiper/modules";
+import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
 const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setReviews(data);
+      });
+  }, []);
   return (
-    <div>
+    <div className="my-5">
       <SectionTitle
         heading={"---What Our Clients Say--- "}
         subHeading={"TESTIMONIALS"}
       ></SectionTitle>
-      <div></div>
+      <div>
+        <Swiper
+          pagination={{
+            type: "",
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className="mx-56 flex flex-col items-center">
+                <Rating
+                  style={{ maxWidth: 180 }}
+                  value={review.rating}
+                  readOnly
+                />
+
+                <p>{review.details}</p>
+                <h3 className="text-warning">{review.name}</h3>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
