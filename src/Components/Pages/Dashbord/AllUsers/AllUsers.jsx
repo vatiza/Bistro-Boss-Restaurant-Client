afa7d6719413e6ffc86434c79c393967170ecb9e
vatiza <rakibur.mac@gmail.com>
2024-05-13 00:50:25 +0600

    user make admin

--- src/Components/Pages/Dashbord/AllUsers/AllUsers.jsx
+++ src/Components/Pages/Dashbord/AllUsers/AllUsers.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
+import toast, { Toaster } from "react-hot-toast";
 import { FaTrashAlt, FaUserShield } from "react-icons/fa";
 
 const AllUsers = () => {
@@ -11,15 +12,39 @@ const AllUsers = () => {
     },
   });
 
-  const handleDelete=(id)=>{
-    console.log(id)
+  const notify = () => toast.success("Successfully Added New Admin!");
+  const notifyDelete = () => toast.error("User Successfully Deleted.");
 
-  }
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/users/${id}`, {
+      method: "delete",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        refetch();
+        if (data.deletedCount) {
+          notifyDelete();
+        }
+      });
+  };
+  const hadnleMakeAdmin = (id) => {
+    fetch(`http://localhost:5000/users/admin/${id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        refetch();
+        if (data.modifiedCount > 0) {
+          notify();
+        }
+      });
+  };
   return (
     <div>
       <Helmet>
         <title>Bistro Boss| All Users</title>
       </Helmet>
+
       <div className="uppercase font-bold     items-center">
         <h2>Total Users: {users.length}</h2>
       </div>
@@ -36,16 +61,25 @@ const AllUsers = () => {
             </tr>
           </thead>
           <tbody>
-           {
-            users.map((user,index)=><tr
-           key={index} >
-                <th>{index+1}</th>
+            {users.map((user, index) => (
+              <tr key={index}>
+                <th>{index + 1}</th>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                <td>{
-                    user.role==='admin'?'admin':
-                    <button className="btn bg-orange-700"><FaUserShield /></button>
-                    }</td>
+                <td>
+                  {user.role === "admin" ? (
+                    "admin"
+                  ) : (
+                    <button
+                      onClick={() => {
+                        hadnleMakeAdmin(user._id);
+                      }}
+                      className="btn bg-orange-700"
+                    >
+                      <FaUserShield />
+                    </button>
+                  )}
+                </td>
                 <td>
                   <button
                     onClick={() => handleDelete(user._id)}
@@ -54,12 +88,12 @@ const AllUsers = () => {
                     <FaTrashAlt></FaTrashAlt>
                   </button>
                 </td>
-              </tr>)
-           }
-            
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
+      <Toaster />
     </div>
   );
 };
