6ad02344bf4fe32024821d7025502fbfd4cf7b18
vatiza <rakibur.mac@gmail.com>
2024-05-16 02:46:44 +0600

    done project

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
 
 const AdminHome = () => {
   const { user } = useAuth();
@@ -12,7 +13,33 @@ const AdminHome = () => {
       return res.data;
     },
   });
-  console.log(stats);
+  //barChart
+  const { data: chartData = [] } = useQuery({
+    queryKey: ["chart-Data"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/order-stats");
+      return res.data;
+    },
+  });
+  console.log(chartData)
+
+  const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
+    Z`;
+  };
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
   return (
     <div className="w-full m-4">
       <h2 className="text-xl">Welcome back, {user.displayName}</h2>
@@ -23,17 +50,46 @@ const AdminHome = () => {
         </div>
 
         <div className="stat">
-          <div className="stat-value">{stats.user}</div>
+          <div className="stat-value">{stats.users}</div>
           <div className="text-3xl">Customers</div>
         </div>
 
         <div className="stat">
-          <div className="stat-value">{stats.products}</div>
+          <div className="stat-value">{stats.menuItems}</div>
           <div className="text-3xl">Products</div>
         </div>
         <div className="stat">
           <div className="stat-value">{stats.orders}</div>
           <div className="text-3xl">Orders</div>
+        </div>
+      </div>
+      <div className="flex bg-white">
+        <div className="w-1/2">
+          <BarChart
+            width={500}
+            height={300}
+            data={chartData}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Bar
+              dataKey="total"
+              fill="#8884d8"
+              shape={<TriangleBar />}
+              label={{ position: "top" }}
+            >
+              {chartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              ))}
+            </Bar>
+          </BarChart>
         </div>
       </div>
     </div>
