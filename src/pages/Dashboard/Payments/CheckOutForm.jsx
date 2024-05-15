9a94a3b7ce07cc30337abc8e9d84ac4b9d3b88b9
vatiza <rakibur.mac@gmail.com>
2024-05-15 11:31:56 +0600

    show error msg

--- src/pages/Dashboard/Payments/CheckOutForm.jsx
+++ src/pages/Dashboard/Payments/CheckOutForm.jsx
@@ -1,8 +1,10 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckOutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const [cardError, setCardError] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -13,30 +15,50 @@ const CheckOutForm = () => {
     if (card === null) {
       return;
     }
-
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+    if (error) {
+      console.log("[error]", error);
+      setCardError(error.message);
+    } else {
+      setCardError("");
+      console.log("[PaymentMethod]", paymentMethod);
+    }
   };
   return (
-    <form className="w-2/4 mt-5 flex flex-col mx-auto" onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
+    <>
+      <form
+        className="w-2/4 mt-5 flex flex-col mx-auto"
+        onSubmit={handleSubmit}
+      >
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
-      <button className="btn btn-warning mt-4" type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
+          }}
+        />
+        <button
+          className="btn btn-warning mt-4"
+          type="submit"
+          disabled={!stripe}
+        >
+          Pay
+        </button>
+      </form>
+      {cardError && <p className="text-red-600 flex flex-col text-xl items-center ">{cardError}</p>}
+    </>
   );
 };
 
